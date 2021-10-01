e49a03163ec19ad79eee7f30ca798edb08b781ba
samiisrafil45 <90086506+samiisrafil45@users.noreply.github.com>
2021-10-01 16:23:42 +0530

    Update hover.js

--- hover.js
+++ hover.js
@@ -2,13 +2,21 @@
 
 
 
-// glow p-block elements on hover
+// *****************************glow ALKALI METALS ON HOVER elements on hover****************************
+
+
+// Getting the element onto which we have to hover
 const selectAlkaliMetals = document.querySelector(".alkali-metals");
 
+// adding event on the above element
 selectAlkaliMetals.addEventListener("mousemove", () => {
 
+    
+   // At first we have to give class named "element" to all the elements and then get all the elements 
     var allElements = document.querySelectorAll(".element");
 
+    // Changing opacity to 0.3 of all the elements with className "element" and opacity to 1 onto which we have already hovered
+    
      allElements.forEach((element) => {
           element.style.opacity = 0.3; 
           selectAlkaliMetals.style.opacity= 1;
@@ -18,7 +26,7 @@ selectAlkaliMetals.addEventListener("mousemove", () => {
 
 
 
-
+// Changing opacity to 1 of all the elements corresponding to selected/hovered feild(alkali metals for now)
     var alkaliEarthMetals= document.querySelectorAll(".alkali-earth-metal-item");
     alkaliEarthMetals.forEach((element) => {
         element.style.opacity = 1; 
@@ -29,14 +37,10 @@ selectAlkaliMetals.addEventListener("mousemove", () => {
     });
 
 
-   
-
-
-
-
-
 });
 
+
+//for mouseout
 selectAlkaliMetals.addEventListener("mouseout", () => {
     let allElements = document.querySelectorAll(".element");
 
